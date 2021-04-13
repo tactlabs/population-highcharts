003ee11531acdfce8765e0de150f510160b4ba8d
Akshara_A_S <aksharaas.aids2020@citchennai.net>
2021-04-13 13:06:41 +0530

    docker files updated

---
+++ app.py
@@ -0,0 +1,63 @@
+from flask import Flask,render_template, jsonify, request
+import random
+import json
+import population
+
+
+app  = Flask(__name__)
+PORT = 3000
+
+@app.route("/", methods=["GET","POST"])
+def startpy():
+    
+    result = {
+
+        "Greetings" : "Tactlabs welcomes you"
+    }
+
+    # return jsonify(result)
+    return render_template("index.html") 
+
+'''
+http://0.0.0.0:3091/api/data
+'''
+
+@app.route("/api/data", methods=["GET"])
+def api_get_data():
+
+    result = population.get_data()
+
+    # result_dict = {
+
+    #     ''       : year,
+    #     'pytorch'    : pytorch,
+    #     'tensorFlow' : tensorFlow
+
+    # }
+
+    return jsonify(result)
+
+'''
+http://0.0.0.0:3091/api/add
+http://0.0.0.0:3091/api/add?year=2017&ontario_tourist=20345&quebec_tourist=200
+http://0.0.0.0:3000/api/add?year=2021&pytorch=180&tensorFlow=90
+'''
+@app.route("/api/add", methods=["GET"])
+def api_add_data():
+
+    region           = request.values.get('region')
+    population         = request.values.get('population')
+   
+    result = {
+        'region'          : region,
+        'population'       : population,
+        
+
+    }
+    result_data = population.add_row(region, population)
+
+    return jsonify(result)
+
+
+if __name__ == "__main__":
+    app.run(debug = True,host="0.0.0.0",port = PORT)
---
+++ population.py
@@ -0,0 +1,49 @@
+import pandas as pd 
+
+def get_data():
+    
+    df = pd.read_csv('/home/akshara/Documents/Intern/population-highchart/data.csv')
+
+    
+    region = df['region'].tolist()
+    population = df['population'].tolist()
+
+    
+      
+    list_of_tuples = [tuple(row) for row in df.values]
+  
+    print(list_of_tuples)
+
+    # print(df['quebec'].tolist())
+
+    result_dict = {
+        'region': region,
+        'population': population,
+        'data_list': list_of_tuples,
+    }
+
+    # print(result_dict)
+
+    return result_dict
+
+def add_row(region,population):
+
+    df = pd.read_csv('/home/akshara/Documents/Intern/population-highchart/data.csv') 
+
+    new_row = {
+    
+        'region'       : region, 
+        'population'    : population, 
+        
+    }
+
+    print(df)
+
+    df = df.append(new_row, ignore_index=True)
+
+    print(df)
+
+    df.to_csv('/home/akshara/Documents/Intern/population-highchart/data.csv')
+
+if __name__ == "__main__":
+    get_data()
